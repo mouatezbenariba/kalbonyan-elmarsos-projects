50c8181218bb9309e27b40db6116b2ac19d7c825
Elmouatez Billah Benariba <78697168+mouatezbenariba@users.noreply.github.com>
2021-09-04 22:43:46 +0100

    exo 04_05

---
+++ Programming Foundations/Fundamentals/Exercises/04_05.py
@@ -0,0 +1,12 @@
+# my favorite food
+fav_food = "chekhchoukha"
+
+food = input("What's my favorite food?")
+
+if fav_food == food:
+    print("Yep! So amazing!")
+else:
+    print("Yuck! that's not it!")
+
+# thank the user
+print("Thanks for playing!")
