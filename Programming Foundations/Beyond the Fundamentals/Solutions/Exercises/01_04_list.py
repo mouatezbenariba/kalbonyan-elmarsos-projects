3582fdd4b2ee85ffafc148205883716938fe0f8e
Elmouatez Billah Benariba <78697168+mouatezbenariba@users.noreply.github.com>
2021-09-16 12:11:57 +0100

    list

---
+++ Programming Foundations/Beyond the Fundamentals/Solutions/Exercises/01_04_list.py
@@ -0,0 +1,15 @@
+# Office locations
+# city1 = 'Tokyo'
+# city2 = 'Dakar'
+# city3 = 'Mumbai'
+# city4 = 'Buenos Aires'
+
+cities = [
+    'Tokyo', 
+    'Dakar', 
+    'Mumbai', 
+    'Buenos Aires',
+]
+
+# print the second item
+print(cities[1])
