2ddf27169d7b86da8298562c2950cb81cc2c8a34
Elmouatez Billah Benariba <78697168+mouatezbenariba@users.noreply.github.com>
2021-09-16 10:46:03 +0100

    convert into a list

--- Programming Foundations/Beyond the Fundamentals/Solutions/Exercises/01_02.py
+++ Programming Foundations/Beyond the Fundamentals/Solutions/Exercises/01_02.py
@@ -1,7 +1,16 @@
 # Office locations
 # variables containing related data
 
-city1 = 'Constantine'
-city2 = 'Cairo'
-city3 = 'New York'
-city4 = 'Paris'
+# city1 = 'Constantine'
+# city2 = 'Cairo'
+# city3 = 'New York'
+# city4 = 'Paris'
+
+# Creating a list
+
+cities = [
+  'Constantine',
+  'Cairo',
+  'New York',
+  'Paris',
+]
