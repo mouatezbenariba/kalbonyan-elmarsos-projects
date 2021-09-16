e7a02ac5d683902c8510104c9e9703acaa97adca
Elmouatez Billah Benariba <78697168+mouatezbenariba@users.noreply.github.com>
2021-09-16 14:13:18 +0100

    challenge solution

---
+++ Programming Foundations/Beyond the Fundamentals/Solutions/Challenges/01_06_challenge_01_working_with_a_collection.py
@@ -0,0 +1,39 @@
+# Nearest stars to Earth
+# star1 = 'Sol'
+# star2 = 'Alpha Centauri'
+# star3 = 'Barnard'
+# star4 = 'Wolf 359'
+
+
+
+# Highest peak on each tectonic plate
+# African = 'Kilimanjaro'
+# Antarctic = 'Vinson'
+# Australian = 'Puncak Jaya'
+# Eurasian = 'Everest'
+# North_American = 'Denali'
+# Pacific = 'Mauna Kea'
+# South_American = 'Aconcagua'
+
+
+# convert stars into a list
+stars = [
+  'Sol',
+  'Alpha Centauri',
+  'Barnard',
+  'Wolf 359',
+]
+
+# convert peaks into a dictionary
+peaks = {
+  'African' : 'Kilimanjaro',
+  'Antarctic' : 'Vinson',
+  'Australian' : 'Puncak Jaya',
+  'Eurasian' : 'Everest',
+  'North_American' : 'Denali',
+  'Pacific' : 'Mauna Kea',
+  'South_American' : 'Aconcagua',
+}
+
+print(stars[3])
+print(peaks['Pacific'])
