1d8a62c69d355db0727ef63f99c176baa5cc5a83
Elmouatez Billah Benariba <78697168+mouatezbenariba@users.noreply.github.com>
2021-09-16 11:58:59 +0100

    replacing the comma by a column

--- Programming Foundations/Beyond the Fundamentals/Solutions/Exercises/01_03.py
+++ Programming Foundations/Beyond the Fundamentals/Solutions/Exercises/01_03.py
@@ -1,5 +1,13 @@
 # California state symbols
-state_bird = 'California quail'
-state_animal = 'Grizzly bear'
-state_flower = 'California poppy'
-state_fruit = 'Avocado'
+# state_bird = 'California quail'
+# state_animal = 'Grizzly bear'
+# state_flower = 'California poppy'
+# state_fruit = 'Avocado'
+
+# Collecting the variables in a dictionnary
+california_symbols = {
+  'bird' : 'California quail',
+  'animal' : 'Grizzly bear',
+  'flower' : 'California poppy',
+  'fruit' : 'Avocado',
+}
