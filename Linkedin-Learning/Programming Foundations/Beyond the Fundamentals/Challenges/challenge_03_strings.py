913f133016dcfb26b3f075042bdce2109e870f5e
Elmouatez Billah Benariba <78697168+mouatezbenariba@users.noreply.github.com>
2021-11-01 15:35:50 +0100

    optimize the code

--- Linkedin-Learning/Programming Foundations/Beyond the Fundamentals/Challenges/challenge_03_strings.py
+++ Linkedin-Learning/Programming Foundations/Beyond the Fundamentals/Challenges/challenge_03_strings.py
@@ -2,5 +2,6 @@
 # kilometers_value = miles_value * 1.609344
 
 miles = input('Enter a distance in miles: ')
-kilometers = float(miles) * 1.609344
+miles_float = float(miles)
+kilometers = miles_float * 1.609344
 print(f'The distance in Kilometers is: {kilometers} km' )
