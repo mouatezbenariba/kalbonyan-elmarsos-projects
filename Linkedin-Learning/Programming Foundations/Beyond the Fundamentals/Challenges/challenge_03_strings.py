1bd8fd054d751afc41b7f0da9bb57d407d7eeaa5
Elmouatez Billah Benariba <78697168+mouatezbenariba@users.noreply.github.com>
2021-11-01 15:30:27 +0100

    correct file names + strings challenge

---
+++ Linkedin-Learning/Programming Foundations/Beyond the Fundamentals/Challenges/challenge_03_strings.py
@@ -0,0 +1,6 @@
+# conversion of miles into kilometers
+# kilometers_value = miles_value * 1.609344
+
+miles = input('Enter a distance in miles: ')
+kilometers = float(miles) * 1.609344
+print(f'The distance in Kilometers is: {kilometers} km' )
