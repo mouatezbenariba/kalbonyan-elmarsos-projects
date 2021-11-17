4e98c86f45381bb388b050b62b598723cc65ebf4
Elmouatez Billah Benariba <78697168+mouatezbenariba@users.noreply.github.com>
2021-11-17 11:58:56 +0100

    excercise 07_05 solution

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Exercises/07_05.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Exercises/07_05.py
@@ -1,7 +1,11 @@
 def check_temp(temp):
-    if  temp > 15:
+    if  temp < 15:
         print('Bring a jacket')
     elif temp > 25 and temp <= 35:
         print('Pack a jacket')
-    elif temp < 35:
+    elif temp > 35:
         print('Leave the jacket at home')
+
+check_temp(10)
+check_temp(30)
+check_temp(37)
