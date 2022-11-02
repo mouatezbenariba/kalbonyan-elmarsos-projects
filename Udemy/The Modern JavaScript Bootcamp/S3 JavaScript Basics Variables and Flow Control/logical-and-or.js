2bc93fd80f55c2f1cda24fbfa6b5c85bf2143cdd
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-11-02 15:09:29 +0100

    add  logical-and-or assignment

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/logical-and-or.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/logical-and-or.js
@@ -1,1 +1,10 @@
-The Modern JavaScript Bootcamp
+let isGuestOneVegan = true;
+let isGuestTwoVegan = true;
+
+if (isGuestOneVegan === true && isGuestTwoVegan === true) {
+  console.log("Only offer up vegan dishes! ")
+} else if (isGuestOneVegan === true || isGuestTwoVegan === true) {
+  console.log("Make sure to offer up some vegan options.")
+} else {
+  console.log("Offer up anything on the menu.")
+}
