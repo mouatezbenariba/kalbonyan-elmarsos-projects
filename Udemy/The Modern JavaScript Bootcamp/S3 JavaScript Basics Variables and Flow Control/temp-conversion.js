556f056201ae5390b406b3dcc5651dafcd84f7e6
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-11-02 15:06:23 +0100

    add tempreture cnversion assignment

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/temp-conversion.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/temp-conversion.js
@@ -1,1 +1,6 @@
-The Modern JavaScript Bootcamp
+let fahrenheit = 32; //0C° - 273.15k°
+let celsius = (fahrenheit - 32) * 5 / 9;
+let kelvin = (fahrenheit + 459.67) * 5 / 9;
+
+console.log(celsius);
+console.log(kelvin);
