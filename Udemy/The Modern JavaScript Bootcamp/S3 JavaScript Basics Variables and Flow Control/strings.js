5869335f309050cc06b6c4cbd302f5f42ed1f444
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-11-02 15:11:32 +0100

    add strings assignment

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/strings.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/strings.js
@@ -1,1 +1,8 @@
-The Modern JavaScript Bootcamp
+let city = "Constantine";
+let country = "Algeria";
+let myLocationOne = city + ", " +country;
+let myLocationTwo = city.concat(", ").concat(country);
+
+console.log(myLocationOne);
+console.log(myLocationTwo);
+
