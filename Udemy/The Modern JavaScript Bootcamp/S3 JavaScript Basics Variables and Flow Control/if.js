7c4e18f2f42ae5a353a1d3edaa512ea5671b54e9
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-11-02 15:10:05 +0100

    add if statement assingment

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/if.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/if.js
@@ -1,1 +1,8 @@
-The Modern JavaScript Bootcamp
+let age = 65;
+
+if (age <= 7) {
+  console.log("The Child gets the meal for free!");
+}
+if (age >= 65) {
+  console.log("The Senior gets 50% discount!");
+}
