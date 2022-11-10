11bc6acc60c40bd318ec22fc5e3dd25ccff9127b
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-11-10 09:50:54 +0100

    add grade calculator

--- Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/grade-calc.js
+++ Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/grade-calc.js
@@ -1,1 +1,19 @@
-The Modern JavaScript Bootcamp
+let gardeCalc = function(studentScore, totalPossibleScore) {
+  let gradePercentage = (studentScore / totalPossibleScore) * 100;
+  let grade;
+  if(gradePercentage <= 59) {
+    grade = "F";
+  } else if (gradePercentage <= 69) {
+    grade = "D";
+  } else if (gradePercentage <= 79){
+    grade = "C"
+  } else if (gradePercentage <= 89) {
+    grade = "B";
+  } else if (gradePercentage <= 100) {
+    grade = "A";
+  }
+  return `You got a ${grade} (${gradePercentage}%)!`
+}
+
+let grade = gardeCalc(15,20);
+console.log(grade);
