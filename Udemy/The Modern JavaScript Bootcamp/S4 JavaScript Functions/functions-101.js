0978750e1ea58a57925320172a97d8a8986f3cce
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-11-10 09:48:04 +0100

    add function basics challenge

--- Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/functions-101.js
+++ Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/functions-101.js
@@ -1,1 +1,7 @@
-The Modern JavaScript Bootcamp
+let fahrenheitToCelsius = function(tempFahrenheit) {
+  let tempCelsius = (tempFahrenheit - 32) * 5 / 9;
+    return tempCelsius;
+}
+
+console.log(fahrenheitToCelsius(32));
+console.log(fahrenheitToCelsius(68));
