3f942fe5fe3f9f6f308d491f983b3394d9faebb0
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-11-15 13:57:41 +0100

    add string methods notes

--- Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/string-methods.js
+++ Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/string-methods.js
@@ -1,1 +1,8 @@
-The Modern JavaScript Bootcamp
+// is valid password
+// length is more than 8 - and it doesn't contain the word password
+
+let isValidPassword = function(password) {
+  return !password.includes("password") && password.length > 8;
+}
+
+console.log(isValidPassword("aaaaaaaaa"));
