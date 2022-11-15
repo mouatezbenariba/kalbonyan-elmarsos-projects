6cecba7e2a0e67b0c3692aa8858625429a47b277
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-11-15 13:55:23 +0100

    add objects basics notes

--- Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/objects-101.js
+++ Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/objects-101.js
@@ -1,1 +1,11 @@
-The Modern JavaScript Bootcamp
+let mouatezInfo = {
+  name: "Elmouatez Billah",
+  age: "24",
+  country:"Algeria",
+}
+
+console.log(`My name is ${mouatezInfo.name}. I am ${mouatezInfo.age} years old. I live in ${mouatezInfo.country}.`)
+
+mouatezInfo.age = "25";
+
+console.log(`My name is ${mouatezInfo.name}. I am ${mouatezInfo.age} years old. I live in ${mouatezInfo.country}.`)
