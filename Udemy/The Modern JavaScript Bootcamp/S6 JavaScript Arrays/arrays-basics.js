27a5d43c6519e9de57eafeffa47e88426e690e3a
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-12-01 20:27:01 +0100

    add arrays basics assignment

---
+++ Udemy/The Modern JavaScript Bootcamp/S6 JavaScript Arrays/arrays-basics.js
@@ -0,0 +1,7 @@
+let toDo= ["wake up at 04:00am", "pray", "do workout","take a shower","go to work"];
+
+console.log(`You have ${toDo.length} todos.`);
+
+for(let i = 0;i < toDo.length;i++) {
+  console.log(`ToDo : ${toDo[i]}`);
+}
