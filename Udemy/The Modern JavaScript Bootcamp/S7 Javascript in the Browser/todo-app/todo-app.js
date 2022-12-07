855d203d22884bcb582495525b09af65fd290907
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-12-07 10:54:37 +0100

    add todo app (052-adding-elements)

--- Udemy/The Modern JavaScript Bootcamp/S7 Javascript in the Browser/todo-app/todo-app.js
+++ Udemy/The Modern JavaScript Bootcamp/S7 Javascript in the Browser/todo-app/todo-app.js
@@ -1,1 +1,30 @@
-The Modern JavaScript Bootcamp
+let toDo = [{
+  text : "wake up at 04:00am",
+  completed : true,
+},{
+  text : "pray",
+  completed : true, 
+},{
+  text : "do workout",
+  completed : false,
+},{
+  text : "take a shower",
+  completed : true,
+},{
+  text : "go to work",
+  completed : false,
+}]
+
+const incompletedTodos = toDo.filter(function(item) {
+  return !item.completed;
+});
+
+const summary = document.createElement("h2");
+summary.textContent = `You have ${incompletedTodos.length} Incompleted todos`;
+document.body.append(summary);
+
+toDo.forEach(function(item) {
+  let todoItem = document.createElement("p");
+  todoItem.textContent = item.text;
+  document.body.append(todoItem);
+})
