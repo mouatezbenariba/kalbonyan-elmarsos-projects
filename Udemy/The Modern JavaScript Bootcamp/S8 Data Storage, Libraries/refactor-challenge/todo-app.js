bcfbb76cb61bc848419df6adf724b09499b1ccee
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-12-31 18:34:46 +0100

    add todo-app js file

---
+++ Udemy/The Modern JavaScript Bootcamp/S8 Data Storage, Libraries/refactor-challenge/todo-app.js
@@ -0,0 +1,23 @@
+let toDo = getSavedTodos();
+
+const filters = {
+  searchText : "",
+}
+
+renderTodos(toDo, filters);
+
+document.querySelector("#search-todos").addEventListener("input", function(e) {
+  filters.searchText =  e.target.value; 
+  renderTodos(toDo, filters); 
+})
+
+document.getElementById("todo-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+    toDo.push({
+    text: e.target.elements.addTodo.value,
+    completed: false,
+  })
+  saveTodos(toDo);
+  renderTodos(toDo, filters); 
+  e.target.elements.addTodo.value = "";
+})
