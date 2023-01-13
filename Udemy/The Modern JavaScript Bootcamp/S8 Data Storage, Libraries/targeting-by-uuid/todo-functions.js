d2dd00657009ba9c25b1c928c629faf1fbbbeffa
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2023-01-13 18:34:40 +0100

    add remove todos functions

--- Udemy/The Modern JavaScript Bootcamp/S8 Data Storage, Libraries/targeting-by-uuid/todo-functions.js
+++ Udemy/The Modern JavaScript Bootcamp/S8 Data Storage, Libraries/targeting-by-uuid/todo-functions.js
@@ -1,3 +1,7 @@
+// 1. Wire up button event
+// 2. Remove todo by id
+// 3. Save and rerender the todos list
+
 // Fetch existing todos from localStorage
 let getSavedTodos = function () {
   const storedInfo = JSON.parse(localStorage.getItem('todos'));
@@ -30,18 +34,18 @@ const renderTodos = function (todos, filter) {
   });
 };
 
-// 1. Setup a root div
-// 2. Setup and append a checkbox (set type attribute)
-// someNode.setAttribute("type","checkbox")
-// 3. Setup and append a span (set text)
-// 4. Setup and append a button (set text)
-
 // Get the DOM elements for an individual note
 let generateTodoDOM = function (item) {
   const todoEl = document.createElement('div');
   const todoCheck = document.createElement('input');
   const todoText = document.createElement('span');
   const button = document.createElement('button');
+
+  button.addEventListener('click', function () {
+    removeTodo(item.id);
+    saveTodos(toDo);
+    renderTodos(toDo, filters);
+  });
 
   // Setup todo checkbox
   todoCheck.setAttribute('type', 'checkbox');
@@ -63,4 +67,15 @@ let generateSummaryDOM = function (incomTodos) {
   return (document.getElementById(
     'todo-list'
   ).innerHTML = `<h2>You have ${incomTodos.length} Incompleted todos</h2>`);
+};
+
+// Remove a Todo from the todos list
+const removeTodo = function (id) {
+  const todoIndex = toDo.findIndex(function (todo) {
+    return todo.id === id;
+  });
+
+  if (todoIndex > -1) {
+    toDo.splice(todoIndex, 1);
+  }
 };
