61ff838493591b727bbc7b3e92c731faca3ee6de
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2023-02-12 16:36:33 +0100

    add: add truthy/falsy assignment files

---
+++ Udemy/The Modern JavaScript Bootcamp/S9 Expanding Our js knowledge/truthy-falsy/notes-app/notes-edit.js
@@ -0,0 +1,52 @@
+const DateTime = luxon.DateTime;
+const noteId = document.location.hash.substring(1);
+const noteTitle = document.querySelector('#note-title');
+const lastEdited = document.querySelector('#last-edited');
+const noteBody = document.querySelector('#note-body');
+const editsRemoveButton = document.querySelector('#remove-note');
+let notes = getSavedNotes();
+let note = notes.find((note) => note.id === noteId);
+
+if (note === undefined) {
+  location.assign('/index.html');
+}
+
+noteTitle.value = note.title;
+noteBody.value = note.body;
+lastEdited.textContent = generateLastEdited(note.updateAt);
+
+noteTitle.addEventListener('input', (e) => {
+  note.title = e.target.value;
+  note.updateAt = DateTime.now().ts;
+  lastEdited.textContent = generateLastEdited(note.updateAt);
+  saveNotes(notes);
+});
+
+noteBody.addEventListener('input', (e) => {
+  note.body = e.target.value;
+  note.updateAt = DateTime.now().ts;
+  lastEdited.textContent = generateLastEdited(note.updateAt);
+  saveNotes(notes);
+});
+
+editsRemoveButton.addEventListener('click', () => {
+  removeNote(note.id);
+  saveNotes(notes);
+  location.assign('/index.html');
+});
+
+window.addEventListener('storage', (e) => {
+  if (e.key === 'notes') {
+    notes = JSON.parse(e.newValue);
+    note = notes.find((note) => note.id === noteId);
+
+    if (note === undefined) {
+      location.assign('/index.html');
+    }
+
+    noteTitle.value = note.title;
+    noteBody.value = note.body;
+    lastEdited.textContent = generateLastEdited(note.updateAt);
+    console.log('test 03');
+  }
+});
---
+++ Udemy/The Modern JavaScript Bootcamp/S9 Expanding Our js knowledge/truthy-falsy/notes-app/notes-functions.js
@@ -0,0 +1,109 @@
+// Read existing notes from localStorage
+const getSavedNotes = () => {
+  const notesJSON = localStorage.getItem('notes');
+  return notesJSON !== null ? JSON.parse(notesJSON) : [];
+};
+
+// Save the notes to localStorage
+const saveNotes = (notes) => {
+  localStorage.setItem('notes', JSON.stringify(notes));
+};
+
+// Remove a note from the list
+const removeNote = (id) => {
+  const noteIndex = notes.findIndex((note) => note.id === id);
+
+  if (noteIndex > -1) {
+    notes.splice(noteIndex, 1);
+  }
+};
+
+// Generate the DOM structure for a note
+const generateNoteDOM = (note) => {
+  const noteEl = document.createElement('div');
+  const textEl = document.createElement('a');
+  const button = document.createElement('button');
+
+  // Setup the remove note button
+  button.textContent = 'x';
+  noteEl.appendChild(button);
+  button.addEventListener('click', function () {
+    removeNote(note.id);
+    saveNotes(notes);
+    renderNotes(notes, filters);
+  });
+
+  // Setup the note title text
+  if (note.title.length > 0) {
+    textEl.textContent = note.title;
+  } else {
+    textEl.textContent = 'Unnamed note';
+  }
+  textEl.setAttribute('href', `/edit.html#${note.id}`);
+  noteEl.appendChild(textEl);
+
+  return noteEl;
+};
+
+const sortNotes = (notes, sortBy) => {
+  if (sortBy === 'byEdited') {
+    return notes.sort(function (a, b) {
+      if (a.updateAt > b.updateAt) {
+        return -1;
+      } else if (a.updateAt < b.updateAt) {
+        return 1;
+      } else if (a.updateAt === b.updateAt) {
+        return 0;
+      }
+    });
+  } else if (sortBy === 'byCreated') {
+    return notes.sort((a, b) => {
+      if (a.createAt > b.createAt) {
+        return -1;
+      } else if (a.createAt < b.createAt) {
+        return 1;
+      } else if (a.createAt === b.createAt) {
+        return 0;
+      }
+    });
+  } else if (sortBy === 'alphabetical') {
+    return notes.sort((a, b) => {
+      if (a.title.toLowerCase() < b.title.toLowerCase()) {
+        return -1;
+      } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+        return 1;
+      } else if (a.title.toLowerCase() === b.title.toLowerCase()) {
+        return 0;
+      }
+    });
+  } else {
+    return notes;
+  }
+};
+
+// Render application notes
+const renderNotes = (notes, filters) => {
+  notes = sortNotes(notes, filters.sortBy);
+  const filteredNotes = notes.filter((note) =>
+    note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+  );
+
+  document.querySelector('#notes').innerHTML = '';
+
+  filteredNotes.forEach((note) => {
+    const noteEl = generateNoteDOM(note);
+    document.querySelector('#notes').appendChild(noteEl);
+  });
+};
+
+// Generate last edited message
+const generateLastEdited = (time) => {
+  var end = DateTime.now();
+  var start = DateTime.fromMillis(time);
+  var timestamp = end.diff(start);
+  if (timestamp > 1000) {
+    return `Last edited ${timestamp.toFormat('s')} seconds ago`;
+  } else if (timestamp < 1000) {
+    return `Last edited few seconds ago`;
+  }
+};
