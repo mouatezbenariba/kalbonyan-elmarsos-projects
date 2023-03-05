bfd84c7645d7c43034265215d733938d582320ce
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2023-03-05 14:38:12 +0100

    add hangman constructor assignment

--- Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/hangman/hangman.js
+++ Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/hangman/hangman.js
@@ -1,1 +1,10 @@
-The Modern JavaScript Bootcamp
+const Hangman = function (word, attempts) {
+  this.word = word;
+  this.attempts = attempts;
+};
+
+const game1 = new Hangman('cat', 2);
+console.log(game1);
+
+const game2 = new Hangman('Constantine', 3);
+console.log(game2);
