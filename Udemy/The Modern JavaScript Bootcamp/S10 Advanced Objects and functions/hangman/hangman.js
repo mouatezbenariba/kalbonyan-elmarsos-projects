5f6e510d0347d1d7bfe6c3349887392df54bfe0d
Mouatez Benariba <benariba.mouatez@gmail.com>
2023-05-22 14:17:50 +0100

    add hangman.js challenge 1 solution (my solution)

--- Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/hangman/hangman.js
+++ Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/hangman/hangman.js
@@ -1,10 +1,28 @@
 const Hangman = function (word, remainingGuesses) {
-  this.word = word;
+  this.word = word.toLowerCase().split('');
   this.remainingGuesses = remainingGuesses;
+  this.guessed = ['a'];
 };
 
-const exampleOne = new Hangman('algeria', 3);
-const exampleTwo = new Hangman('constantine', 5);
+Hangman.prototype.getPuzzle = function () {
+  let puzzle = '';
+  this.guessed.forEach((guess) => {
+    this.word.forEach((letter) => {
+      if (letter === guess) {
+        puzzle += guess;
+      } else {
+        puzzle += '*';
+      }
+    });
+  });
 
-console.log(exampleOne);
-console.log(exampleTwo);
+  return `${puzzle}`;
+};
+
+const game1 = new Hangman('Cat', 2);
+console.log(game1);
+console.log(game1.getPuzzle());
+
+const game2 = new Hangman('New Jersey', 4);
+console.log(game2);
+console.log(game2.getPuzzle());
