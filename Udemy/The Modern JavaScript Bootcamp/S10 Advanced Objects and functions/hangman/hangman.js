ad56e3928d73fa9a8362cd17186d11c29cb5ad35
Mouatez Benariba <benariba.mouatez@gmail.com>
2023-05-24 19:15:40 +0100

    update hangman game to accept guesses

--- Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/hangman/hangman.js
+++ Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/hangman/hangman.js
@@ -1,7 +1,7 @@
 const Hangman = function (word, remainingGuesses) {
   this.word = word.toLowerCase().split('');
   this.remainingGuesses = remainingGuesses;
-  this.guessed = ['c'];
+  this.guessed = [];
 };
 
 Hangman.prototype.getPuzzle = function () {
@@ -15,13 +15,33 @@ Hangman.prototype.getPuzzle = function () {
     }
   });
 
-  return `${puzzle}`;
+  return `${puzzle}, it remains ${this.remainingGuesses} guesses.`;
 };
 
-const game1 = new Hangman('Cat', 2);
-console.log(game1);
+Hangman.prototype.guessPuzzle = function (guessedLetter) {
+  // accept a character for guessing
+  let list = guessedLetter.split(',');
+
+  // add unique guesses to the list of guesses
+  list.forEach((item) => {
+    if (!this.guessed.includes(item)) {
+      this.guessed.push(item);
+    }
+  });
+
+  // decrement the guesses left if a unique guess isn't a match
+  this.remainingGuesses -= this.guessed.length;
+
+  return this.guessed;
+};
+
+const game1 = new Hangman('Cat', 5);
+console.log(game1.guessPuzzle('c,d,c'));
 console.log(game1.getPuzzle());
 
-const game2 = new Hangman('New Jersey', 4);
-console.log(game2);
-console.log(game2.getPuzzle());
+// console.log(game1);
+// console.log(game1.getPuzzle());
+
+// const game2 = new Hangman('New Jersey', 4);
+// console.log(game2);
+// console.log(game2.getPuzzle());
