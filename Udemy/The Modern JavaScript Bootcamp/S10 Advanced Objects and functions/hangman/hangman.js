0aa245d07c6e94b0decaa6c5c4c2916285f4b24f
Mouatez Benariba <benariba.mouatez@gmail.com>
2023-05-25 15:55:04 +0100

    add class syntax to hangman game

--- Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/hangman/hangman.js
+++ Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/hangman/hangman.js
@@ -1,60 +1,60 @@
-const Hangman = function (word, remainingGuesses) {
-  this.word = word.toLowerCase().split('');
-  this.remainingGuesses = remainingGuesses;
-  this.guessedLetters = [];
-  this.status = 'playing';
-};
-
-Hangman.prototype.getPuzzle = function () {
-  let puzzle = '';
-
-  this.word.forEach((letter) => {
-    if (this.guessedLetters.includes(letter) || letter === ' ') {
-      puzzle += letter;
-    } else {
-      puzzle += '*';
-    }
-  });
-
-  return `${puzzle}`;
-};
-
-Hangman.prototype.makeGuess = function (guess) {
-  guess = guess.toLowerCase();
-  const isUnique = !this.guessedLetters.includes(guess);
-  const isBadGuess = !this.word.includes(guess);
-
-  if (isUnique) {
-    this.guessedLetters.push(guess);
-  }
-
-  if (isUnique && isBadGuess) {
-    this.remainingGuesses--;
-  }
-
-  this.recalculateGameStatus();
-
-  console.log(this.recalculateGameStatus());
-};
-
-Hangman.prototype.recalculateGameStatus = function () {
-  if (game1.getPuzzle().includes('*') && this.remainingGuesses > 0) {
+class Hangman {
+  constructor(word, remainingGuesses) {
+    this.word = word.toLowerCase().split('');
+    this.remainingGuesses = remainingGuesses;
+    this.guessedLetters = [];
     this.status = 'playing';
-  } else if (!game1.getPuzzle().includes('*') && this.remainingGuesses > 0) {
-    this.status = 'finished';
-  } else if (game1.getPuzzle().includes('*') && this.remainingGuesses <= 0) {
-    this.status = 'failed';
   }
 
-  return this.status;
-};
+  getPuzzle() {
+    let puzzle = '';
 
-Hangman.prototype.statusMessage = function () {
-  if (game1.status == 'playing') {
-    return `${game1.getPuzzle()}. Guesses left: ${game1.remainingGuesses}`;
-  } else if (game1.status == 'failed') {
-    return `${game1.getPuzzle()}. Nice try! The word was: "${game1.word.join('')}"`;
-  } else if (game1.status == 'finished') {
-    return `${game1.getPuzzle()}. Great work! You guessed the word.`;
+    this.word.forEach((letter) => {
+      if (this.guessedLetters.includes(letter) || letter === ' ') {
+        puzzle += letter;
+      } else {
+        puzzle += '*';
+      }
+    });
+
+    return `${puzzle}`;
   }
-};
+
+  makeGuess(guess) {
+    guess = guess.toLowerCase();
+    const isUnique = !this.guessedLetters.includes(guess);
+    const isBadGuess = !this.word.includes(guess);
+
+    if (isUnique) {
+      this.guessedLetters.push(guess);
+    }
+
+    if (isUnique && isBadGuess) {
+      this.remainingGuesses--;
+    }
+
+    this.recalculateGameStatus();
+  }
+
+  recalculateGameStatus() {
+    if (game1.getPuzzle().includes('*') && this.remainingGuesses > 0) {
+      this.status = 'playing';
+    } else if (!game1.getPuzzle().includes('*') && this.remainingGuesses > 0) {
+      this.status = 'finished';
+    } else if (game1.getPuzzle().includes('*') && this.remainingGuesses <= 0) {
+      this.status = 'failed';
+    }
+
+    return this.status;
+  }
+
+  statusMessage() {
+    if (game1.status == 'playing') {
+      return `${game1.getPuzzle()}. Guesses left: ${game1.remainingGuesses}`;
+    } else if (game1.status == 'failed') {
+      return `${game1.getPuzzle()}. Nice try! The word was: "${game1.word.join('')}"`;
+    } else if (game1.status == 'finished') {
+      return `${game1.getPuzzle()}. Great work! You guessed the word.`;
+    }
+  }
+}
