27ddd16b7a2fadc1c0ad55bcb9813b8a7fb89cab
Mouatez Benariba <benariba.mouatez@gmail.com>
2023-06-01 14:35:06 +0100

    update hangman second solution

--- Udemy/The Modern JavaScript Bootcamp/S11 Asynchronous js/hangman/app.js
+++ Udemy/The Modern JavaScript Bootcamp/S11 Asynchronous js/hangman/app.js
@@ -21,13 +21,19 @@ const request = new XMLHttpRequest();
 request.open('GET', 'https://restcountries.com/v3.1/all');
 request.send();
 
+countryCode = 'DZ';
+
 request.addEventListener('readystatechange', (e) => {
   if (e.target.readyState === 4 && e.target.status === 200) {
     const data = JSON.parse(e.target.responseText);
-    data.forEach((country) => {
-      if (country.cca2 === 'DZ') {
-        console.log(country.name.official);
-      }
-    });
+    // data.forEach((country) => {
+    //   if (country.cca2 === countryCode) {
+    //     console.log(country.name.official);
+    //   }
+    // });
+
+    const country = data.find((country) => country.cca2 === countryCode);
+
+    console.log(country.name.official);
   }
 });
